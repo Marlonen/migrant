f917e9fbc2ec3f599010579eff16779d662f22f3
comger <comger@gmail.com>
2013-11-03 12:26:47 +0800

    add news save

--- web/action/news.py
+++ web/action/news.py
@@ -5,7 +5,7 @@
 """
 from kpages import url
 from utility import BaseHandler
-
+from logic.news import NewsModel
 
 @url(r'/news?')
 class News(BaseHandler):
@@ -17,3 +17,16 @@ class News(BaseHandler):
 class NewsInfo(BaseHandler):
     def get(self,_id=None):
         self.render('action/newsinfo.html')
+
+@url(r'/news/create')
+class CreateNews(BaseHandler,NewsModel):
+    def post(self):
+        try:
+            data = self._get_postdata()
+            _id = self._save(data)
+            self.write(dict(status=True,data=_id)) 
+        except Exception as e:
+            self.write(e.message)
+
+    def get(self):
+        self.render('action/newssave.html')
