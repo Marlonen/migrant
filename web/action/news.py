76e13d61eed07c049c64e773b9216c7ac8a90550
comger <comger@gmail.com>
2013-11-03 21:05:20 +0800

    add create news

--- web/action/news.py
+++ web/action/news.py
@@ -6,6 +6,8 @@
 from kpages import url
 from utility import BaseHandler
 from logic.news import NewsModel
+from logic.category import TName as T_Category
+from logic.utility import m_page
 
 @url(r'/news?')
 class News(BaseHandler):
@@ -22,11 +24,16 @@ class NewsInfo(BaseHandler):
 class CreateNews(BaseHandler,NewsModel):
     def post(self):
         try:
-            data = self._get_postdata()
-            _id = self._save(data)
-            self.write(dict(status=True,data=_id)) 
+            data = self._get_postdata(author=self.uid)
+            r,v = self._save(data)
+            self.write(dict(status=r,data=v)) 
         except Exception as e:
             self.write(e.message)
 
     def get(self):
+        self.labels = ['码农'.decode('utf-8'),'码码代']
+        r,self.categorys = m_page(T_Category,size=10)        
+        print self.categorys 
         self.render('action/newssave.html')
+
+
---
+++ web/restful/labels.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*- 
+
+"""
+    comment action 
+    author comger@gmail.com
+"""
+import json
+from kpages import url
+from utility import RestfulHandler
+from logic.utility import m_page
+
+@url(r'/m/labels/suggest/(.*)')
+class SuggestLabel(RestfulHandler):
+    def get(self, category=None):
+        arr = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+        self.write(dict(status=True,data=arr))
