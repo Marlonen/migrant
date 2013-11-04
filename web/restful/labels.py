e3691b81016fe9dcd28d1c6659b27572e327d70a
comger <comger@gmail.com>
2013-11-04 22:45:47 +0800

    add ajax label

--- web/restful/labels.py
+++ web/restful/labels.py
@@ -5,15 +5,17 @@
     author comger@gmail.com
 """
 import json
+from tornado.web import RequestHandler
 from kpages import url
 from utility import RestfulHandler
 from logic.utility import m_page
 
 @url(r'/m/label/suggest/(.*)')
-class SuggestLabel(RestfulHandler):
-    def get(self, category=None):
+class SuggestLabel(RequestHandler):
+    def post(self, category=None):
         arr = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-        self.write(dict(status=True,data=arr))
+        self.write(json.dumps(arr))
+
 
 @url(r'/m/label/add')
 class AddLabel(RestfulHandler):
