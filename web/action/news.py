04d741325dbb4eb29619393ea969d105d132114f
comger <comger@gmail.com>
2013-11-14 17:16:54 +0800

    add intro for news

--- web/action/news.py
+++ web/action/news.py
@@ -4,6 +4,7 @@
     migrant 前端展示公共页面
 """
 import datetime
+import re
 from kpages import url
 from utility import BaseHandler
 from logic.news import NewsModel,hot,TName as TNews
@@ -11,6 +12,8 @@ from logic.category import TName as T_Category
 from logic.account import TName as T_Account
 from logic.utility import m_page,m_info
 from logic.label import add as addlabel
+
+strip_tag_pat=re.compile('</?\w+[^>]*>')
 
 @url(r'/news?')
 class News(BaseHandler):
@@ -22,7 +25,8 @@ class News(BaseHandler):
         news = {}
         for cate in self.categorys:
             r,news[cate.get('listname')] = m_page(TNews,category=cate.get('listname'))
-
+        
+        
         self.news = news
         self.render('action/news.html')
 
@@ -47,7 +51,8 @@ class CreateNews(BaseHandler,NewsModel):
         try:
             addon = datetime.datetime.now()
             data = self._get_postdata(addon=addon)
-            print data
+            intro = re.sub(strip_tag_pat,' ',data.get('body',''))
+            data['intro'] = intro[0:100]
             r,v = self._save(data)
             for i in data.get('labels',()):
                 if i:
