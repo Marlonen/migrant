bb6b66d0beedbcae50b3ec09831bbc00316054ff
comger <comger@gmail.com>
2014-05-08 08:34:22 +0800

    fix kindeditor ajax sub bugs

--- web/static/metro/js/frm.js
+++ web/static/metro/js/frm.js
@@ -2,7 +2,7 @@
 	author comger@gmail.com
 	全局方法
 **/
-
+var editor;
 function select_nav(classname){
 	$('#nav-list li').removeClass('open').removeClass('active');
 	$('#nav-list li li').removeClass('active');
@@ -14,7 +14,7 @@ function select_nav(classname){
 }
 
 $(function(){
-	var editor;
+
 	KindEditor.ready(function(K) {
 		editor = K.create('textarea.KindEditor', {
 			resizeType : 1,
@@ -27,7 +27,7 @@ $(function(){
 			items : [
 				'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic', 'underline',
 				'removeformat', '|', 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist',
-				'insertunorderedlist', '|', 'emoticons', 'image', 'insertfile' , 'baidumap', 'link', '|', 'template','code','quickformat','clearhtml','|','source','fullscreen']
+				'insertunorderedlist', '|', 'emoticons', 'image', 'insertfile' , 'baidumap', 'link', '|','code','quickformat','clearhtml','|','source','fullscreen']
 		});
 	});
 	
